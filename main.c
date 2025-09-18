359dcbb18772ed1489f1530a592d40067e0cce66
Maximus <mkorobchenko21@gmail.com>
2025-09-18 22:39:13 +0300

    getted modify
    fixed truble, added check functions, max value of area, conditioinals

--- main.c
+++ main.c
@@ -5,25 +5,36 @@
 #define PI 3.141592
 
 int main(void) {
-    float a, b, c, r, S_triangle, S_circle;
+    float a, b, c, r, S_triangle, S_semicircle, max_area;
+    const char *max_figure;
 
     //input a
-    printf("Enter the side \"a\" of the triangle: ");
+    printf("Enter the side \"a\" of the triangle:\n");
     scanf("%f", &a);
 
     //input b
-    printf("Enter the side \"b\" of the triangle: ");
+    printf("Enter the side \"b\" of the triangle:\n");
     scanf("%f", &b);
 
     //calculating
-    c = sqrt(a*a + b*b);
-    r = c/2;
-    S_triangle = (a + b)/2;
-    S_circle = (PI*r*r)/2;
+    c = sqrt(a * a + b * b);
+    r = c / 2;
+    S_triangle = (a * b) / 2;
+    S_semicircle = (PI * r * r) / 2;
+
+
+    if (S_triangle > S_semicircle) {
+        printf("The area of triangle is bigger\n");
+    }
+    else {
+        printf("The area of semicircle is bigger\n");
+    }
+
 
     //output
     printf("The area of the triangle is %.2f\n", S_triangle);
-    printf("The area of the circle is %.2f\n", S_circle);
+    printf("The area of semicircle is %.2f\n", S_semicircle);
+    //printf("The area of the max figure (%s) %.2f\n", max_figure, max_area);
     getch();
     return 0;
 }
