b54d2e483050edb6566b99c52614891000ec2545
Maximus <mkorobchenko21@gmail.com>
2025-09-18 15:45:13 +0300

    First version of the program
    Added input, calculation and output

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+#define PI 3.141592
 
 int main(void) {
-    printf("Hello, World!\n");
+    float a, b, c, r, S_triangle, S_circle;
+
+    //input a
+    printf("Enter the side \"a\" of the triangle: ");
+    scanf("%f", &a);
+
+    //input b
+    printf("Enter the side \"b\" of the triangle: ");
+    scanf("%f", &b);
+
+    //calculating
+    c = sqrt(a*a + b*b);
+    r = c/2;
+    S_triangle = (a + b)/2;
+    S_circle = (PI*r*r)/2;
+
+    //output
+    printf("The area of the triangle is %.2f\n", S_triangle);
+    printf("The area of the circle is %.2f\n", S_circle);
+    getch();
     return 0;
 }
